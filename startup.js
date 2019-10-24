a03ab03441d92d78144368188849239939a04b59
M-Samoht <t.melesi@gmail.com>
2019-10-24 23:47:51 +0200

    Update startup.js

--- startup.js
+++ startup.js
@@ -1,10 +1,10 @@
 console.log(`
 min_peers = ${process.env.MIN_PEER || "1"}
 max_peers = ${process.env.MAX_PEER || "100"}
-min_peers_seed = ${process.env.MIN_PEER_SEED || "1"}
-max_peers_seed = ${process.env.MAX_PEER_SEED || "100"}
-max_uploads = ${process.env.MAX_UPLOAD || "10"}
-max_downloads_global = ${process.env.MAX_DOWNLOAD || "10"}
+min_peers_seed = ${process.env.MIN_PEERS_SEED || "1"}
+max_peers_seed = ${process.env.MAX_PEERS_SEED || "100"}
+max_uploads = ${process.env.MAX_UPLOADS || "10"}
+max_downloads_global = ${process.env.MAX_DOWNLOADS_GLOBAL || "10"}
 download_rate = ${process.env.DOWNLOAD_RATE || "0"}
 upload_rate = ${process.env.UPLOAD_RATE || "0"}
 directory = ${process.env.DIRECTORY || "/data"}
